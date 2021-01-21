248ebd2d98fb50ea2fa2fb718cad8646396443eb
Tatjana Svjatova <tsv.mailbox@gmail.com>
2021-01-21 17:30:27 +0200

    useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import Final from "./tutorial/4-forms/final/2d-multiple-inputs-Component";
-import Project from "./tutorial/4-forms/corresponding-projects/3-Grocery-bud/Main";
+import Final from "./tutorial/5-useRef/final/1-useRef-basics";
+// import Project from "./tutorial/4-forms/corresponding-projects/3-Grocery-bud/Main";
 function App() {
   return (
     <div className="container">
       <h2>Advanced Tutorial</h2>
-      {/* <Final /> */}
-      <Project />
+      <Final />
+      {/* <Project /> */}
     </div>
   );
 }
--- src/tutorial/5-useRef/final/1-useRef-basics.js
+++ src/tutorial/5-useRef/final/1-useRef-basics.js
@@ -1,28 +1,48 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
-// preserves value
+//UseRef:
+// for uncontrolled inputs
+// preserves value in between renders
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
+//1
 const UseRefBasics = () => {
+  //2.3
+  //set up useRef:with initial value
   const refContainer = useRef(null);
-
+  //2.2
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(refContainer.current.value);
+    //logged: input value
   };
+  // console.log(refContainer);
+  //--------------------
+  //2.4
+  //as useRef don't cause rerender,
+  //use useEffect without dependencies
   useEffect(() => {
     console.log(refContainer.current);
+    //logged: <input type="text">
     refContainer.current.focus();
   });
-
+  //2
   return (
     <>
-      <form className='form' onSubmit={handleSubmit}>
+      <form
+        className="form"
+        //2.1
+        onSubmit={handleSubmit}
+      >
         <div>
-          <input type='text' ref={refContainer} />
+          <input
+            type="text"
+            //2.4 use refContainer as attribute "ref"
+            ref={refContainer}
+          />
         </div>
-        <button type='submit'>submit</button>
+        <button type="submit">submit</button>
       </form>
     </>
   );
