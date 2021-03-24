909e43f9d52deb491de7672c7b89153b0556b492
Tatjana Svjatova <tsv.mailbox@gmail.com>
2021-03-24 17:51:43 +0200

    check

--- src/tutorial/2a-fetch-useState-useEffect/corresponding-projects/1-Stock-Photos/Main.js
+++ src/tutorial/2a-fetch-useState-useEffect/corresponding-projects/1-Stock-Photos/Main.js
@@ -5,30 +5,47 @@ import "./styles.css";
 
 //EnvVar
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+//first part of url for default images
 const mainUrl = `https://api.unsplash.com/photos/`;
+//first part of url for searched images
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 const Main = () => {
+  //state
   const [loading, setLoading] = useState(false);
+  //list of photos (empty array)
   const [photos, setPhotos] = useState([]);
+  //
   const [page, setPage] = useState(0);
   const [query, setQuery] = useState("");
 
   //--------------------
+  //fetch data
   const fetchImages = async () => {
     setLoading(true);
+    //default images or searched images
     let url;
 
     const urlPage = `&page=${page}`;
     const urlQuery = `&query=${query}`;
 
     if (query) {
+      //searched images:
+      //searchUrl=first part of url
+      //clientID=
+      //urlPage=
+      //urlQuery=
       url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
     } else {
+      //default images
+      //mainUrl = first part of url
+      //clientID=
+      //urlPage=
       url = `${mainUrl}${clientID}${urlPage}`;
     }
 
     try {
+      //fetch
       const response = await fetch(url);
       const data = await response.json();
 
@@ -48,6 +65,7 @@ const Main = () => {
     }
   };
   //--------------------
+  //run when app is loading or 'page' is changed
   useEffect(() => {
     fetchImages();
   }, [page]);
