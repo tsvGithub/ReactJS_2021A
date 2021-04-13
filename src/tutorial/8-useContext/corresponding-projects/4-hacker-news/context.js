857e92a71f273c79c6a9622ed55825a65a885bbb
Tatjana Svjatova <tsv.mailbox@gmail.com>
2021-04-13 18:02:58 +0300

    improved reducer actions context

--- src/tutorial/8-useContext/corresponding-projects/4-hacker-news/context.js
+++ src/tutorial/8-useContext/corresponding-projects/4-hacker-news/context.js
@@ -16,18 +16,17 @@ const initialState = {
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  //reducer function + initial state
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const fetchStories = async (url) => {
-    //Loading in 'Stories' component
-    //dispatch Loading
     dispatch({ type: SET_LOADING });
     try {
       const response = await fetch(url);
       const data = await response.json();
-
-      dispatch({ type: SET_STORIES, payload: { hits: data.hits, nbPages: data.nbPages } });
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      });
     } catch (error) {
       console.log(error);
     }
@@ -36,31 +35,23 @@ const AppProvider = ({ children }) => {
   const removeStory = (id) => {
     dispatch({ type: REMOVE_STORY, payload: id });
   };
-
   const handleSearch = (query) => {
     dispatch({ type: HANDLE_SEARCH, payload: query });
   };
-
   const handlePage = (value) => {
     dispatch({ type: HANDLE_PAGE, payload: value });
   };
-
-  //
   useEffect(() => {
     fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
   }, [state.query, state.page]);
 
   return (
-    <AppContext.Provider
-      //
-      value={{ ...state, removeStory, handleSearch, handlePage }}
-    >
-      {children}
-    </AppContext.Provider>
+    <AppContext.Provider value={{ ...state, removeStory, handleSearch, handlePage }}>{children}</AppContext.Provider>
   );
 };
+// make sure use
 export const useGlobalContext = () => {
-  return useContext(AppProvider);
+  return useContext(AppContext);
 };
 
 export { AppContext, AppProvider };
--- src/tutorial/8-useContext/corresponding-projects/4-hacker-news/reducer.js
+++ src/tutorial/8-useContext/corresponding-projects/4-hacker-news/reducer.js
@@ -5,21 +5,27 @@ const reducer = (state, action) => {
     case SET_LOADING:
       return { ...state, isLoading: true };
     case SET_STORIES:
-      return { ...state, isLoading: false, hits: action.payload.hits, nbPages: action.payload.nbPages };
+      return {
+        ...state,
+        isLoading: false,
+        hits: action.payload.hits,
+        nbPages: action.payload.nbPages,
+      };
     case REMOVE_STORY:
-      return { ...state, hits: state.hits.filter((story) => story.objectID !== action.payload) };
+      return {
+        ...state,
+        hits: state.hits.filter((story) => story.objectID !== action.payload),
+      };
     case HANDLE_SEARCH:
       return { ...state, query: action.payload, page: 0 };
     case HANDLE_PAGE:
       if (action.payload === "inc") {
         let nextPage = state.page + 1;
-
         if (nextPage > state.nbPages - 1) {
           nextPage = 0;
         }
         return { ...state, page: nextPage };
       }
-
       if (action.payload === "dec") {
         let prevPage = state.page - 1;
         if (prevPage < 0) {
@@ -28,7 +34,7 @@ const reducer = (state, action) => {
         return { ...state, page: prevPage };
       }
     default:
-      throw new Error(`no matching "${action.type}" action type`);
+      throw new Error(`no mathching "${action.type}" action type`);
   }
 };
 export default reducer;
