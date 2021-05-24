aa3c9e87488b5166148124eabcc622061237e27b
Tatjana Svjatova <tsv.mailbox@gmail.com>
2021-05-24 16:07:23 +0300

    checkWinner with useEffect

--- src/tutorial/8-useContext/corresponding-projects/6-roll-dice/context.js
+++ src/tutorial/8-useContext/corresponding-projects/6-roll-dice/context.js
@@ -28,7 +28,7 @@ const AppProvider = ({ children }, props) => {
   // let [pirmais, otrais] = totalScore;
   // console.log({ pirmais }, { otrais });
   //{pirmais: 0} {otrais: 0}
-  // const [isPlaying, setIsPlaying] = useState(totalScore <= 101 ? true : false);
+  // const [isPlaying, setIsPlaying] = useState(totalScoreOb[activePlayer] <= 10 ? true : false);
   const [isPlaying, setIsPlaying] = useState(true);
   //----------------
   //(1) new Game:
@@ -42,16 +42,9 @@ const AppProvider = ({ children }, props) => {
     setDice(null); //null
     console.log(`NewGame In Context "dice" is ${dice}`); //null
     //---------------
-    //!!!
-    // setActivePlayer(0);
-    // // console.log(`In Context ActivePlayer is ${activePlayer}`); //
-    // console.log(`In Context ActivePlayer is ${setActivePlayer(players[0])}`); //
     setActivePlayer(1); //0
-    // setActivePlayer(0); //0
     console.log(`NewGame In Context ActivePlayer is ${activePlayer}`); //
     //--------
-    // setCurrentScore([]); //0
-    // setCurrentScore(0); //0
     setCurrentScore({ 1: 0, 2: 0 }); //0
     console.log(`NewGame In Context current score is ${currentScore}`); //
     setTotalScoreOb({ 1: 0, 2: 0 }); //0
@@ -101,7 +94,16 @@ const AppProvider = ({ children }, props) => {
     // setActivePlayer(activePlayer === 0 ? 1 : 0);
     console.log(`SwitchPlayer In context: Active Player swiched to ${activePlayer}`);
   };
-
+  //=============================
+  const checkWinner = () => {
+    if (totalScoreOb[activePlayer] >= 21) {
+      console.log(`totalScore of activePlayer is ${totalScoreOb[activePlayer]}`);
+      console.log(`Congrats! The winner is ${activePlayer}`);
+      setIsPlaying(false);
+    } else {
+      switchPlayer();
+    }
+  };
   //(2) Roll Dice
   const rollDice = () => {
     if (isPlaying) {
@@ -145,64 +147,35 @@ const AppProvider = ({ children }, props) => {
   //-----------------
   //3) Hold
   const holdDice = () => {
-    // console.log(totalScore[activePlayer]); //
-    // setTotalScore((score) => [...score, (totalScore[activePlayer] += currentScore[activePlayer])]);
-    console.log(`HoldDice ActivePlayer is ${activePlayer}`); //0 //1
-    console.log(`HoldDice TotalScore is ${{ totalScoreOb }}`); //[0,0] //0
-    console.log(`HoldDice CurrentScore is ${currentScore}`); //estj
-    console.log(`HoldDice TotalScore of ActivePlayer is ${totalScoreOb[activePlayer]}`); //0//undefined
+    // console.log(`HoldDice ActivePlayer is ${activePlayer}`);
+    // console.log(`HoldDice TotalScore is ${{ totalScoreOb }}`);
+    // console.log(`HoldDice CurrentScore is ${currentScore}`);
+    // console.log(`HoldDice TotalScore of ActivePlayer is ${totalScoreOb[activePlayer]}`);
 
     let itogo = totalScoreOb[activePlayer] + currentScore[activePlayer];
     console.log(`HoldDice 'Itogo': ${itogo}`);
     setTotalScoreOb(activePlayer === 1 ? { ...totalScoreOb, 1: itogo } : { ...totalScoreOb, 2: itogo });
-    // setTotalScoreOb({ ...totalScoreOb, 2: itogo });
-    // setTotalScore(itogo);
-    // setTotalScore({ totalScore: [...totalScore, itogo] });
-    // setTotalScore({ ...totalScore, itogo });
-    // setTotalScore({ totalScore: { itogo } });
-    //--------------------
-    //     Update array
-    // const handleUpdate = (index, todo) => {
-    //   const newTodos = [...todos];
-    //   newTodos[index] = todo;
-    //   setTodos(newTodos);
-    // }
-    // //--------------------
-    // // 1. Make a shallow copy of the totalScore
-    // let totals = [...totalScore];
-    // console.log(totals); //[0:18, 1:20]
-    // // 2. Make a shallow copy of the item you want
-    // //to mutate
-    // // let total = { ...totals[0] };
-    // let total = { ...totals[activePlayer] };
-    // console.log(total); //{}
-    // // 3. Replace the property you're intested in
-    // total = itogo;
-    // console.log(total); //18
-    // // 4. Put it back into the array.
-    // //N.B. we *are* mutating the array here, but that's why we made a copy first
-    // // total.total = itogo;
-    // totals[0] = total;
-    // // console.log(totals[0]); //18
-    // console.log(totals[activePlayer]); //18
-    // // 5. Set the state to our new copy
-    // setTotalScore({ totalScore: totals });
-    // console.log(totalScore); //[10,20]
-    // setTotalScore({ ...totalScore, totalScore[activePlayer] = itogo });
-    // console.log(`HoldDice 'TotalScore' after 'itogo' added is ${totalScore}`);
-    // setTotalScore((totalScore[activePlayer] += currentScore));
-    // setTotalScore((score) => [...score, (totalScore[activePlayer] += currentScore)]);
-
-    // setTotalScore((totalScore[activePlayer] += currentScore[activePlayer]));
-    // setTotalScore[activePlayer]((totalScore += currentScore));
-    // setTotalScore((totalScore += currentScore));
-    // console.log(`HoldDice In context 'totalScore' is ${totalScore[activePlayer]}`);
     console.log(`HoldDice 'totalScore' is ${totalScoreOb}`);
-    //0
-    //?
-    // setCurrentScore(0);
-    switchPlayer();
+
+    // checkWinner();
+    // if (totalScoreOb[activePlayer] >= 21) {
+    //   console.log(`Congrats! The winner is ${activePlayer}`);
+    //   setIsPlaying(false);
+    // } else {
+    //   switchPlayer();
+    // }
   };
+  useEffect(() => {
+    checkWinner();
+    // if (totalScoreOb[activePlayer] >= 21) {
+    //   console.log(`totalScore of activePlayer is ${totalScoreOb[activePlayer]}`);
+    //   console.log(`Congrats! The winner is ${activePlayer}`);
+    //   setIsPlaying(false);
+    // } else {
+    //   switchPlayer();
+    // }
+  }, [totalScoreOb]);
+
   //===============
   return (
     <AppContext.Provider
